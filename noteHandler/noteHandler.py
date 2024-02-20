1bf6ee876dc08bec73b34262def784e98fdef7d6
Brian May <brian.may@atomicobject.com>
2024-02-20 18:49:35 -0500

    Intial pass at a state machine via a dictionary

--- noteHandler/noteHandler.py
+++ noteHandler/noteHandler.py
@@ -4,22 +4,52 @@ from utils.singleton import Singleton
 from wrappers.wrapperedSparkMax import WrapperedSparkMax
 
 
-class NoteSystem:
-    stateApproachingNote = "DrivingTowards"
-    stateDockingTransfer = "EntersTransfer"
-    stateExitingTransfer = "ExitedTransfer"
-    stateStoppingForward = "HaltingForward"
-    stateReversingAction = "ReverseActions"
-    stateStoppingReverse = "HaltingReverse"
-    stateShooterPrepared = "ShooterPrepped"
+class NoteStateMachine:
+
+    def __init__(self):
+        self.initial_state = "idle"
+        self.current_state = self.initial_state
+        self.initState_transition_endState = {
+            "idle": {
+                "initiateIntake": "intaking"
+            },
+            "intaking": {
+                "noteEnteredTransfer": "enteringTransfer",
+                "cancelIntake": "idle"
+            },
+            "enteringTransfer": {
+                "noteExitedTransfer": "exitingTransfer"
+            },
+            "exitingTransfer": {
+                "noteInRightSpot": "readyToShoot",
+                "noteNeedsAdjustment": "reverseTransferAdjustment"
+            },
+            "reverseTransferAdjustment": {
+                "noteInRightSpot": "readyToShoot"
+            },
+            "readyToShoot": {
+                "shooterTriggered": "preppingShooter"
+            },
+            "preppingShooter": {
+                "shooterReady": "shooting"
+            },
+            "shooting": {
+                "noteShot": "idle"
+            }
+        }
+
+    def transition(self, transition):
+        if transition in self.initState_transition_endState[self.current_state]:
+            self.current_state = self.initState_transition_endState[self.current_state][transition]
+        else:
+            raise Exception(f'Invalid transition from {self.current_state} with transtion {transition}')
 
 
 class NoteHandler(metaclass=Singleton):
     def __init__(self):
+        self.stateMachine = NoteStateMachine()
         # initiate motors, etc
         self.motor = WrapperedSparkMax()
 
-        current_state = NoteSystem.stateApproachingNote4
-
-        if current_state == NoteSystem.stateShooterPrepared
-
+    def update(self):
+        self.stateMachine.transition("initiateIntake")
