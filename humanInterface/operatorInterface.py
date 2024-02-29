4b069097f5ef5ac778b88542d787af778d466743
Brian May <brian.may@atomicobject.com>
2024-02-28 21:15:39 -0500

    Remove note intake manual controls to free up operator controls

--- humanInterface/operatorInterface.py
+++ humanInterface/operatorInterface.py
@@ -17,11 +17,6 @@ class OperatorInterface:
 
         self.climberCmd = False
 
-        # Only used for manual testing
-        self.manualIntakeVelFactor = 0.0
-        self.manualTransferVelFactor = 0.0
-        self.manualShooterVelFactor = 0.0
-
     def update(self):
         """Main update - call this once every 20ms"""
 
@@ -29,16 +24,6 @@ class OperatorInterface:
             self.startIntake = self.ctrl.getYButtonPressed()
             self.startShooter = self.ctrl.getAButtonPressed()
             self.cancelNoteHandling = self.ctrl.getBButtonPressed()
-
-            # Intake = Left stick 
-            # Transfer = Right stick 
-            # Shooter = Left stick + right bumper
-            intakeRaw = -1.0 * self.ctrl.getLeftY() if not self.ctrl.getRightBumper() else 0.0
-            transferRaw = -1.0 * self.ctrl.getRightY()
-            shooterRaw = -1.0 * self.ctrl.getLeftY() if self.ctrl.getRightBumper() else 0.0
-            self.manualIntakeVelFactor = applyDeadband(intakeRaw, 0.05)
-            self.manualTransferVelFactor = applyDeadband(transferRaw, 0.05)
-            self.manualShooterVelFactor = applyDeadband(shooterRaw, 0.05)
 
             self.climberCmd = self.ctrl.getRightBumper()
 
@@ -49,10 +34,6 @@ class OperatorInterface:
             self.cancelNoteHandling = False
 
             self.climberCmd = False
-
-            self.manualIntakeVelFactor = 0.0
-            self.manualTransferVelFactor = 0.0
-            self.manualShooterVelFactor = 0.0
 
             self.connectedFault.setFaulted()
 
@@ -65,10 +46,3 @@ class OperatorInterface:
 
     def getClimberCmd(self):
         return self.climberCmd
-
-    def getManualIntakeVelocityFactory(self):
-        return self.manualIntakeVelFactor
-    def getManualTransferVelocityFactory(self):
-        return self.manualTransferVelFactor
-    def getManualShooterVelocityFactory(self):
-        return self.manualShooterVelFactor
