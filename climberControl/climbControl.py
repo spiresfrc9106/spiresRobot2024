8e318ba07c92ebad9a78f25cddbdb34e627f1871
Brian May <brian.may@atomicobject.com>
2024-03-09 13:52:54 -0500

    Both sides of climber zero together

--- climberControl/climbControl.py
+++ climberControl/climbControl.py
@@ -1,6 +1,7 @@
 from wpimath.system.plant import DCMotor
 from wrappers.wrapperedSparkMax import WrapperedSparkMax
 from utils.singleton import Singleton
+from utils.signalLogging import log
 from utils.units import RPM2RadPerSec
 
 CLIMB_REVS_PER_INCH = 18.92 / 7.84
@@ -27,8 +28,8 @@ class ClimberMotorControl():
         # And use joysticks to ensure other factors are correct
         # Then test out the zeroing
         self.hasZeroed = "yes"
+        self.otherSideIsWaiting = False
 
-        # TODO: this spark max can id should be moved to a constants file
         self.motor = WrapperedSparkMax(canID, f"_climber{name}", brakeMode=True, curLimitA=5)
         self.motor.setInverted(inverted)
         self.motor.setPID(kP=1.5e-4, kI=0.0, kD=0.0)
@@ -50,12 +51,18 @@ class ClimberMotorControl():
             self.velCmdFraction = 0.0
         elif self.hasZeroed == "zeroing":
             if self.motor.getMotorVelocityRadPerSec() > -ZERO_OUT_RAD_PER_SEC * 0.05:
-                if self.count >= 25:
+                if self.count >= 25: # stopped moving for a half second
                     self.count = 0
-                    self.motor.setVoltage(0.0)
-                    self.changeHasZeroed("yes")
+                    self.changeHasZeroed("awaitingOtherSide")
                 else:
                     self.count += 1
+        elif self.hasZeroed == "awaitingOtherSide":
+            if self.otherSideIsWaiting:
+                self.motor.setVoltage(0.0)
+                self.changeHasZeroed("yes")
+                self.otherSideIsWaiting = False
+            elif self.motor.getMotorVelocityRadPerSec() < -ZERO_OUT_RAD_PER_SEC * 0.05:
+                self.changeHasZeroed("zeroing")
         elif self.hasZeroed == "yes":
             finalVelCmdFraction = self.velCmdFraction
             if abs(finalVelCmdFraction) <= 0.01:
@@ -67,6 +74,8 @@ class ClimberMotorControl():
             # todo we shouldn't get here.
             self.motor.setVoltage(0.0)
             self.velCmdFraction = 0
+
+        log(f'Climber Control Zeroed {self.name}', self.hasZeroed == "yes", "bool")
         self.motor.getVelRPS()
         self.motor.getAppliedOutput()
 
@@ -75,11 +84,17 @@ class ClimberMotorControl():
 
     def resetHasZeroed(self):
         self.hasZeroed = "no"
+        self.otherSideIsWaiting = False
+
+    def setOtherSideIsWaiting(self, otherIsWaiting: bool):
+        self.otherSideIsWaiting = otherIsWaiting
+    def isWaiting(self):
+        return self.hasZeroed == "awaitingOtherSide"
 
 
 class ClimberControl(metaclass=Singleton):
     def __init__(self):
-        # TODO: this spark max can id should be moved to a constants file
+        # TODO: these spark max can IDs should be moved to a constants file
         self.motorLeft = ClimberMotorControl(name='left', inverted=True, canID=16)
         self.motorRight = ClimberMotorControl(name='right', inverted=True, canID=14)
         self.climbCmdPercentage = 0.0
@@ -87,6 +102,10 @@ class ClimberControl(metaclass=Singleton):
     def update(self):
         self.motorLeft.setVelCmdPercentage(self.climbCmdPercentage)
         self.motorRight.setVelCmdPercentage(self.climbCmdPercentage)
+
+        self.motorRight.setOtherSideIsWaiting(self.motorLeft.isWaiting())
+        self.motorLeft.setOtherSideIsWaiting(self.motorRight.isWaiting())
+
         self.motorLeft.update()
         self.motorRight.update()
 
